b1be8ac57eed2a936a4891ecaba2f3dd81a87d9c
tuan anh <tuanah252@gmail.com>
2023-05-29 17:55:09 +0700

    first commit

---
+++ Assets/destroyed.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject pop;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("rabbit")) // Kiểm tra va chạm với đối tượng B
+        {
+            Destroy(gameObject); // Hủy bỏ đối tượng A
+        }
+    }
+     
+
+    void OnDestroy()
+    {
+        Instantiate(pop, transform.position, Quaternion.identity);
+    }
+}
